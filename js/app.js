e6aeae44ae5ee6778b95b4695844724b48db044a
antoinebiogeau <72401464+antoinebiogeau@users.noreply.github.com>
2022-03-24 07:30:37 +0100

    update js

--- js/app.js
+++ js/app.js
@@ -1,7 +1,13 @@
 document.addEventListener("DOMContentLoaded", () => {
     console.log("DOM loaded");
-    let img;
+    let img, home, onclick;
     img = document.querySelector("header #toggle");
+    home = document.querySelector("header img");
+    modale = document.querySelector(".parent-modale");
+    //aller a index.html
+    home.addEventListener("click", () => {
+        window.location.href = "index.html";
+    });
     //dark mode
     img.addEventListener("click", () => {
         if (img.innerHTML === `<img src="./assets/sun_weather_icon_152003.png" alt="" id="theme">`) {
@@ -14,5 +20,41 @@ document.addEventListener("DOMContentLoaded", () => {
             img.classList.add("rota2");
         }
         document.body.classList.toggle("light");
+        document.modale.classList.toggle("light");
+    });
+    console.log(navigator.userAgent);
+
+    let el, modal, closed, open_modal, closed_all;
+
+    el = document.querySelectorAll(".grid-picture-large li");
+    modal = document.querySelector(".parent-modale");
+    closed = document.querySelector(".modale button");
+    closed_all = document.querySelector(".modale img");
+
+    /* property elements */
+
+    open_modal = function () {
+        console.log(this.dataset);
+        /* les variables */
+        let image = this.dataset.image;
+        let title = this.dataset.title;
+        let desc = this.dataset.description;
+        let dates = this.dataset.dates;
+        modal.classList.add("modale-active"); /* ajouter la classe active */
+        /* sélectionner les sélecteurs html*/
+        document.querySelector(".modale img").setAttribute("src", image);
+        document.querySelector(".modale .desc h3").innerText = title;
+        document.querySelector(".modale .desc p").innerHTML = `<strong>Déscription : </strong>${desc}`;
+        document.querySelector(".modale .desc time").innerText = `Annee ${dates}`;
+        document.querySelector(".modale .desc time").setAttribute("datetime", dates);
+    };
+    for (rows of el) {
+        rows.addEventListener("click", open_modal);
+    }
+    closed.addEventListener("click", () => {
+        modal.classList.remove("modale-active");
+    });
+    closed_all.addEventListener("click", () => {
+        modal.classList.remove("modale-active");
     });
 });
