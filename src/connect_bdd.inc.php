aafddb7f5917dce2d005d92b2ab909255eac0c83
antoinebiogeau <72401464+antoinebiogeau@users.noreply.github.com>
2022-05-24 10:06:54 +0200

    debug

--- src/connect_bdd.inc.php
+++ src/connect_bdd.inc.php
@@ -2,10 +2,10 @@
 try {
         $_pdo_options[PDO::ATTR_ERRMODE] = PDO::ERRMODE_EXCEPTION;
         $_bdd = new PDO(
-            'mysql:host=172.190.1.52;
-            dbname=abiogeau',
-            'abiogeau',
-            'azerty',
+            'mysql:host=localhost;
+            dbname=ppeweb',
+            'root',
+            '',
             array(PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8', $_pdo_options)
         );
         echo "Connexion à la base de données réussie";
